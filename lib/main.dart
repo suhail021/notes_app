def1e6ded4ed996e211f29971bed245864feabc9
sohilalngm10@gmail.com <sohilalngm10@gmail.com>
2025-06-07 10:34:43 -0700

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+void main() async {
+  runApp(StoreApp());
+}
+
+class StoreApp extends StatelessWidget {
+  const StoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+    );
+  }
+}
