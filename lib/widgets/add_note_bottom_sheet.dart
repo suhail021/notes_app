8f9645c32e50e20e272934ee56f7e9c5e759e168
sohilalngm10@gmail.com <sohilalngm10@gmail.com>
2025-06-09 13:51:37 -0700

    Add add note cubit to add note bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,73 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:google/widgets/customebuttom.dart';
-import 'package:google/widgets/customtextfileds.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:google/widgets/add_note_form.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-      child: SingleChildScrollView(child: AddNoteForm()),
-    );
-  }
-}
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({super.key});
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  final GlobalKey<FormState> formkey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  String? title, suptitle;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formkey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          CustomTextfileds(
-            hintext: "Title",
-            inputType: TextInputType.text,
-            colorborder: Color(0xff8A8A8A),
-            colorhint: Color.fromARGB(255, 30, 95, 85),
-            maxline: 1,
-            onSaved: (value) {
-              title = value;
-            },
-          ),
-          const SizedBox(height: 10),
-          CustomTextfileds(
-            hintext: "Content",
-            inputType: TextInputType.multiline,
-            colorborder: Color(0xff8A8A8A),
-            colorhint: Color.fromARGB(255, 30, 95, 85),
-            maxline: 6,
-            onSaved: (value) {
-              suptitle = value;
-            },
-          ),
-          const SizedBox(height: 60),
-          Customebuttom(
-            text: "Add",
-            color: Color.fromARGB(255, 30, 95, 85),
-            onTap: () {
-              if (formkey.currentState!.validate()) {
-                formkey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-          ),
-          const SizedBox(height: 8),
-        ],
+      child: SingleChildScrollView(
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteFailure) {
+              print("faillied ${state.errMessage}" );
+            }
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: AddNoteForm(),
+            );
+          },
+        ),
       ),
     );
   }
---
+++ lib/widgets/add_note_form.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/widgets.dart';
+import 'package:google/widgets/customebuttom.dart';
+import 'package:google/widgets/customtextfileds.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({super.key});
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formkey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, suptitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formkey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          CustomTextfileds(
+            hintext: "Title",
+            inputType: TextInputType.text,
+            colorborder: Color(0xff8A8A8A),
+            colorhint: Color.fromARGB(255, 30, 95, 85),
+            maxline: 1,
+            onSaved: (value) {
+              title = value;
+            },
+          ),
+          const SizedBox(height: 10),
+          CustomTextfileds(
+            hintext: "Content",
+            inputType: TextInputType.multiline,
+            colorborder: Color(0xff8A8A8A),
+            colorhint: Color.fromARGB(255, 30, 95, 85),
+            maxline: 6,
+            onSaved: (value) {
+              suptitle = value;
+            },
+          ),
+          const SizedBox(height: 60),
+          Customebuttom(
+            text: "Add",
+            color: Color.fromARGB(255, 30, 95, 85),
+            onTap: () {
+              if (formkey.currentState!.validate()) {
+                formkey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(height: 8),
+        ],
+      ),
+    );
+  }
+}
