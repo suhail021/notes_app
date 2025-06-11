a3c012d534b797de9912f7b859867c7c3227f746
sohilalngm10@gmail.com <sohilalngm10@gmail.com>
2025-06-10 17:50:13 -0700

    refactor the edite note

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -6,6 +6,7 @@ import 'package:google/models/note_model.dart';
 import 'package:google/widgets/color_item.dart';
 import 'package:google/widgets/custom_app_bar.dart';
 import 'package:google/widgets/customtextfileds.dart';
+import 'package:google/widgets/edite_notes_colors_list.dart';
 
 class EditNoteView extends StatefulWidget {
   static String id = "EditNoteView";
@@ -69,50 +70,6 @@ class _EditNoteViewState extends State<EditNoteView> {
             EditeNoteColorsList(note: widget.note),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class EditeNoteColorsList extends StatefulWidget {
-  const EditeNoteColorsList({super.key, required this.note});
-  final NoteModel note;
-  @override
-  State<EditeNoteColorsList> createState() => _EditeNoteColorsListState();
-}
-
-class _EditeNoteColorsListState extends State<EditeNoteColorsList> {
- late int currentIndex;
-  @override
-  void initState() {
-currentIndex =kcolors.indexOf(Color(widget.note.color));
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 36 * 2,
-      child: ListView.builder(
-        itemCount: kcolors.length,
-
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(right: 8),
-            child: GestureDetector(
-              onTap: () {
-                currentIndex = index;
-                widget.note.color = kcolors[index].value;
-                setState(() {});
-              },
-              child: ColorItem(
-                color: kcolors[index],
-                isActive: currentIndex == index,
-              ),
-            ),
-          );
-        },
       ),
     );
   }
--- lib/widgets/edite_notes_colors_list.dart
+++ lib/widgets/edite_notes_colors_list.dart
@@ -3,3 +3,47 @@ import 'package:google/constants.dart';
 import 'package:google/models/note_model.dart';
 import 'package:google/widgets/color_item.dart';
 
+
+class EditeNoteColorsList extends StatefulWidget {
+  const EditeNoteColorsList({super.key, required this.note});
+  final NoteModel note;
+  @override
+  State<EditeNoteColorsList> createState() => _EditeNoteColorsListState();
+}
+
+class _EditeNoteColorsListState extends State<EditeNoteColorsList> {
+ late int currentIndex;
+  @override
+  void initState() {
+currentIndex =kcolors.indexOf(Color(widget.note.color));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 36 * 2,
+      child: ListView.builder(
+        itemCount: kcolors.length,
+
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                widget.note.color = kcolors[index].value;
+                setState(() {});
+              },
+              child: ColorItem(
+                color: kcolors[index],
+                isActive: currentIndex == index,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
